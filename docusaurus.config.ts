eb427a81dcc583e32590c2d2f7393e59d789d999
Callan Barrett <callan@zoocar.org>
2025-03-14 20:07:12 +0800

    Set up for gh pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
 
   organizationName: "ZaparooProject",
   projectName: "zaparoo.org",
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
