6e39eeb6d93d6fa2d7bb720a3835fab313565933
Callan Barrett <callan@zoocar.org>
2025-03-14 20:35:01 +0800

    Temp base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   url: "https://zaparoo.org",
-  baseUrl: "/",
+  baseUrl: "/zaparoo.org/", //"/",
 
   organizationName: "ZaparooProject",
   projectName: "zaparoo.org",
