cbc6a03fefdd5eafa60c0201c797ec3e3ce65df9
Callan Barrett <callan@zoocar.org>
2025-09-13 19:16:08 +0800

    Update showcase

--- src/components/Showcase/index.tsx
+++ src/components/Showcase/index.tsx
@@ -14,6 +14,88 @@ interface FeaturedPhoto extends Photo {
 
 const allPhotos: FeaturedPhoto[] = [
   {
+    src: "/img/showcase/tunnotron3000_gekisha_boy.jpg",
+    width: 900,
+    height: 1200,
+    alt: "Gekisha Boy NFC card. Credit: tunnotron3000 @ Discord",
+  },
+  {
+    src: "/img/showcase/tunnotron3000_bubsy.jpg",
+    width: 900,
+    height: 1200,
+    alt: "Bubsy NFC card. Credit: tunnotron3000 @ Discord",
+    featured: true,
+  },
+  {
+    src: "/img/showcase/flexbone_arcade.jpg",
+    width: 1200,
+    height: 904,
+    alt: "Arcade setup with NFC cards. Credit: flexbone @ Discord",
+  },
+  {
+    src: "/img/showcase/conehead_stacks.png",
+    width: 1200,
+    height: 906,
+    alt: "Stacked NFC card collection. Credit: conehead @ Discord",
+    featured: true,
+  },
+  {
+    src: "/img/showcase/Zag_steam.jpg",
+    width: 1048,
+    height: 1200,
+    alt: "Steam Deck NFC card. Credit: Zag @ Discord",
+  },
+  {
+    src: "/img/showcase/Turbotobi79_zelda.jpg",
+    width: 560,
+    height: 1200,
+    alt: "Legend of Zelda NFC card. Credit: Turbotobi79 @ Discord",
+  },
+  {
+    src: "/img/showcase/MechaRebecca_cassette_cases.jpg",
+    width: 1200,
+    height: 606,
+    alt: "Cassette case NFC card storage. Credit: MechaRebecca @ Discord",
+  },
+  {
+    src: "/img/showcase/MSouza3D_custom_reader.jpg",
+    width: 675,
+    height: 1200,
+    alt: "Custom 3D printed NFC reader. Credit: MSouza3D @ Discord",
+    featured: true,
+  },
+  {
+    src: "/img/showcase/LoVeMaKeRz_psx_mini.jpg",
+    width: 1200,
+    height: 900,
+    alt: "3D printed mini PlayStation console and discs. Credit: LoVeMaKeRz @ Discord",
+    featured: true,
+  },
+  {
+    src: "/img/showcase/LoVeMaKeRz_paprium.jpg",
+    width: 1200,
+    height: 900,
+    alt: "Paprium NFC cart. Credit: LoVeMaKeRz @ Discord",
+  },
+  {
+    src: "/img/showcase/BigBlue709_tgfx.jpg",
+    width: 900,
+    height: 1200,
+    alt: "TurboGrafx-16 NFC cards. Credit: BigBlue709 @ Discord",
+  },
+  {
+    src: "/img/showcase/Anime0t4ku_genesis.jpg",
+    width: 1200,
+    height: 672,
+    alt: "Sega Genesis NFC cards. Credit: Anime0t4ku @ Discord",
+  },
+  {
+    src: "/img/showcase/Anime0t4ku_gb_cart.jpg",
+    width: 961,
+    height: 1200,
+    alt: "Game Boy NFC cart with custom label. Credit: Anime0t4ku @ Discord",
+  },
+  {
     src: "/img/showcase/bten_floppy.jpg",
     width: 1167,
     height: 768,
@@ -30,7 +112,6 @@ const allPhotos: FeaturedPhoto[] = [
     width: 1200,
     height: 901,
     alt: "3D printed mini NeoGeo console and carts. Credit: LoVeMaKeRz @ Discord",
-    featured: true,
   },
   {
     src: "/img/showcase/BFOOT_mini_nes_2.jpg",
@@ -49,7 +130,6 @@ const allPhotos: FeaturedPhoto[] = [
     width: 900,
     height: 1200,
     alt: "3D printed mini NES booth. Credit: BFOOT @ Discord",
-    featured: true,
   },
   {
     src: "/img/showcase/BigBlue709_switch_case.jpg",
@@ -62,7 +142,6 @@ const allPhotos: FeaturedPhoto[] = [
     width: 1200,
     height: 1120,
     alt: "Arcade stick with built-in NFC reader. Credit: PIXEL Memories @ Discord",
-    featured: true,
   },
   {
     src: "/img/showcase/lovemakerz_mini_snes.jpeg",
@@ -183,7 +262,6 @@ const allPhotos: FeaturedPhoto[] = [
     width: 1200,
     height: 900,
     alt: "Zaparoo arcade cab setup. Credit: zodduska @ Discord",
-    featured: true,
   },
   {
     src: "/img/showcase/discord20.jpg",
@@ -193,11 +271,14 @@ const allPhotos: FeaturedPhoto[] = [
   },
 ];
 
-export default function Showcase(props: { limit?: number; featured?: boolean }) {
+export default function Showcase(props: {
+  limit?: number;
+  featured?: boolean;
+}) {
   let photos = allPhotos;
 
   if (props.featured) {
-    photos = allPhotos.filter(photo => photo.featured);
+    photos = allPhotos.filter((photo) => photo.featured);
   }
 
   if (props.limit) {
@@ -210,7 +291,7 @@ export default function Showcase(props: { limit?: number; featured?: boolean }) 
   const albumProps = props.featured
     ? {
         targetRowHeight: 200,
-        rowConstraints: { singleRowMaxHeight: 300 }
+        rowConstraints: { singleRowMaxHeight: 300 },
       }
     : {};
 
@@ -220,7 +301,7 @@ export default function Showcase(props: { limit?: number; featured?: boolean }) 
         photos={photos}
         onClick={({ index }) => setIndex(index)}
         componentsProps={{
-          image: { loading: "lazy" }
+          image: { loading: "lazy" },
         }}
         {...albumProps}
       />
